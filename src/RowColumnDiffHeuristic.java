bc6261f609be388b86e5702be7ff5e2edd3c0fc4
Carlos <toxzcp@gmail.com>
2017-03-20 22:46:50 +0800

    add new heuristic

---
+++ src/RowColumnDiffHeuristic.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Carlos on 3/20/17.
+ */
+public class RowColumnDiffHeuristic implements AStarImplement.Heuristic {
+    private int ord[] = null;
+
+    @Override
+    public int estimate(Square start, Square goal) {
+        if (ord == null) {
+            ord = new int[9];
+            int[] array = goal.array;
+            for (int i = 0; i < array.length; i++) {
+                ord[array[i]] = i;
+            }
+        }
+
+        int cost = 0;
+        for (int i = 0; i < start.array.length; i++) {
+            int num = start.array[i];
+            if (start.array[i] != 0) {
+                if ((i % 3) != ord[num] % 3)// column
+                    cost++;
+                if ((i / 3) != ord[num] / 3)// row
+                    cost++;
+            }
+        }
+        return cost;
+    }
+}
