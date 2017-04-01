22bc3877b8c1b96bff9ec070a64c84391e40ed58
Carlos <toxzcp@gmail.com>
2017-04-01 11:01:11 +0800

    add script to convert dot to png add output README

---
+++ dots/dot2png.sh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/bash
+
+while read file; do
+    dirname=$(dirname "$file")
+    filename=$(basename "$file")
+    filename="${filename%.*}"
+
+    name="$dirname""/""$filename"".png"
+    printf "process file %s...    " "$name"
+    dot -Tpng "$file" -o "$name" & wait
+    printf "OK\n"
+
+
+done < <(find . -iname "*.dot")
